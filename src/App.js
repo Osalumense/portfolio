1ab70f5e4efbe6ad40d82ff8e2dd150df9116e20
Akugbe Stephen <harkugbeosaz@gmail.com>
2022-01-18 06:10:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 
 function App() {
   return (
-    
-<!-- Section 1 -->
 <section className="px-2 pt-32 bg-white md:px-0">
     <div className="container items-center max-w-6xl px-5 mx-auto space-y-6 text-center">
         <h1 className="text-4xl font-extrabold tracking-tight text-left text-gray-900 sm:text-5xl md:text-6xl md:text-center">
