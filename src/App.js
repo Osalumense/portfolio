17c4d997b4c201e951c8a075d528d8f19874bb1c
Akugbe Stephen <harkugbeosaz@gmail.com>
2022-01-18 06:21:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './index.css';
 import './App.css';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 function App() {
   return (
 <section className="px-2 pt-32 bg-white md:px-0">
     <div className="container items-center max-w-6xl px-5 mx-auto space-y-6 text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight text-left text-gray-900 sm:text-5xl md:text-6xl md:text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight text-center lg:text-left text-gray-900 sm:text-5xl md:text-6xl md:text-center">
             <span className="block">I'm currently working on something
              <span className="block mt-1 text-purple-500 lg:inline lg:mt-0">Awesome</span></span>
         </h1>
@@ -14,13 +14,13 @@ function App() {
             Please keep checking for updates
         </p>
         <div className="relative flex flex-col justify-center md:flex-row md:space-x-4">
-            <Link to="https://linkedin.com/in/akugbe-stephen" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-purple-500 rounded-md md:mb-0 hover:bg-purple-700 md:w-auto">
+            <a href="https://linkedin.com/in/akugbe-stephen" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-purple-500 rounded-md md:mb-0 hover:bg-purple-700 md:w-auto">
               Check me on LinkedIn
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <line x1="5" y1="12" x2="19" y2="12"></line>
                     <polyline points="12 5 19 12 12 19"></polyline>
                 </svg>
-            </Link>
+            </a>
             {/* <a href="#_" className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
                 Learn More
             </a> */}
