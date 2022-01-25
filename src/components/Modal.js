06e7d2d98550b9efce136c0352f6e27375ca43d4
Osalumense <harkugbeosaz@gmail.com>
2022-01-25 17:10:51 +0100

    Projects section gradually taking shape

---
+++ src/components/Modal.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Modal = ({canShow, updateModalState, props}) => {
+
+    if (canShow) {
+        return (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            // onClick={updateModalState}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    {props.title}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={updateModalState}
+                  >
+                    <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      x
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    {props.description}
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-between p-6 border-t border-solid border-blueGray-200 rounded-b">
+                    <a href={props.ghLink}>View on GitHub</a>
+                    <a href={props.liveLink}>View Demo</a>
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={updateModalState}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={updateModalState}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-slate-800 dark:bg-yellow-400"></div>
+        </>
+        );
+    }
+  return null;
+}
+
+export default Modal;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,150 +1,139 @@
 import React from 'react';
-import Img from '../assets/images/portfolio-img.png';
+import { ModalContext } from "./Context";
+import Modal from '../components/Modal';
+// import Img from '../assets/images/portfolio-img.png';
 
 const Projects = () => {
+   const [showModal, updateShowModal] = React.useState(false);
+   const toggleModal = () => updateShowModal(state => !state);
+   const projects = [
+      {
+         id: 1,
+         title: 'Motivational Quotes App',
+         description: 'hdhdhdsdhsdj hsdjsdhsjdhsdj h   sdjsdhsjdhsd j hsdjhsdjsdh jshdsjdhsd',
+         ghLink: 'https://github.com/Osalumense/motivational-quotes-app',
+         liveLink: 'https://motivational-quotes-app.vercel.app/'
+
+      }
+   ]
     return (
-        <section className=" dark:bg-slate-900 mx-auto pt-20 pb-10 lg:pb-20 bg-amber-300">
-   <div className="">
-      <div className="flex flex-wrap">
-         <div className="w-full md:w-1/2 xl:w-1/3 px-4 hover:scale-90 transition ease-in-out duration-500">
-            <div className="rounded-lg overflow-hidden mb-10">
-            <div className="p-4 text-center">
-                  <h3>
-                     <h3 className="text-3xl font-bold text-slate-900 dark:text-yellow-400">
-                        Motivational Quotes App
-                     </h3>
-                  </h3>
-                  <p className="text-base font-medium text-body-color leading-relaxed my-4 text-slate-900 dark:text-yellow-400">
-                     This application uses quotes API to display random motivational quotes.
-                  </p>
-                  <a
-                     href="/"
-                     className="
-                     inline-block
-                     py-2
-                     px-7
-                     border border-[#E5E7EB]
-                     rounded-full
-                     text-base text-body-color
-                     font-medium
-                     hover:border-primary hover:bg-primary hover:text-white
-                     transition
-                     "
-                     >
-                  View Details
-                  </a>
-               </div>
-               <img
-                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
-                  alt="cards"
-                  className="w-full rounded-lg"
-                  />
-               
-            </div>
-         </div>
-         <div className="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div className="bg-white rounded-lg overflow-hidden mb-10">
-               <img
-                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-02.jpg"
-                  alt="cards"
-                  className="w-full"
-                  />
-               <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
-                  <h3>
-                     <a
-                        href="/"
-                        className="
-                        font-semibold
-                        text-dark text-xl
-                        sm:text-[22px]
-                        md:text-xl
-                        lg:text-[22px]
-                        xl:text-xl
-                        2xl:text-[22px]
-                        mb-4
-                        block
-                        hover:text-primary
-                        "
-                        >
-                     The ultimate UX and UI guide to card design
-                     </a>
-                  </h3>
-                  <p className="text-base text-body-color leading-relaxed mb-7">
-                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                     elit. Lorem consectetur adipiscing elit.
-                  </p>
-                  <a
-                     href="/"
-                     className="
-                     inline-block
-                     py-2
-                     px-7
-                     border border-[#E5E7EB]
-                     rounded-full
-                     text-base text-body-color
-                     font-medium
-                     hover:border-primary hover:bg-primary hover:text-white
-                     transition
-                     "
-                     >
-                  View Details
-                  </a>
-               </div>
-            </div>
-         </div>
-         <div className="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div className="bg-white rounded-lg overflow-hidden mb-10">
-               <img
-                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-03.jpg"
-                  alt="cards"
-                  className="w-full"
-                  />
-               <div className="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
-                  <h3>
-                     <a
-                        href="/"
-                        className="
-                        font-semibold
-                        text-dark text-xl
-                        sm:text-[22px]
-                        md:text-xl
-                        lg:text-[22px]
-                        xl:text-xl
-                        2xl:text-[22px]
-                        mb-4
-                        block
-                        hover:text-primary
-                        "
-                        >
-                     Creative Card Component designs graphic elements
-                     </a>
-                  </h3>
-                  <p className="text-base text-body-color leading-relaxed mb-7">
-                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                     elit. Lorem consectetur adipiscing elit.
-                  </p>
-                  <a
-                     href="/"
-                     className="
-                     inline-block
-                     py-2
-                     px-7
-                     border border-[#E5E7EB]
-                     rounded-full
-                     text-base text-body-color
-                     font-medium
-                     hover:border-primary hover:bg-primary hover:text-white
-                     transition
-                     "
-                     >
-                  View Details
-                  </a>
+      <ModalContext.Provider value={{ showModal, toggleModal, projects }}>
+         <section className=" dark:bg-slate-900 mx-auto pt-20 pb-10 lg:pb-20 bg-amber-300">
+            <div className="">
+               <div className="flex flex-wrap">
+                  <div className="w-full md:w-1/2 xl:w-1/3 px-4 hover:scale-90 transition ease-in-out duration-500">
+                     <div className="rounded-lg overflow-hidden mb-10">
+                     <div className="p-4 text-center">
+                           <h3>
+                              <h3 className="text-3xl font-bold text-slate-900 dark:text-yellow-400">
+                                 Motivational Quotes App
+                              </h3>
+                           </h3>
+                           <p className="text-base font-medium text-body-color leading-relaxed my-4 text-slate-900 dark:text-yellow-400">
+                              This application uses quotes API to display random motivational quotes.
+                           </p>
+                           <button
+                              className="
+                              inline-block
+                              py-2
+                              px-7
+                              border border-[#E5E7EB]
+                              rounded-full
+                              text-base text-body-color
+                              font-medium
+                              hover:border-primary hover:bg-primary hover:text-white
+                              transition
+                              "
+                              onClick={toggleModal}
+                              >
+                           View Details
+                           </button>
+                        </div>
+                        <img
+                           src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                           alt="cards"
+                           className="w-full rounded-lg"
+                           />
+                        
+                     </div>
+                  </div>
+                  <div className="w-full md:w-1/2 xl:w-1/3 px-4 hover:scale-90 transition ease-in-out duration-500">
+                     <div className="rounded-lg overflow-hidden mb-10">
+                     <div className="p-4 text-center">
+                           <h3>
+                              <h3 className="text-3xl font-bold text-slate-900 dark:text-yellow-400">
+                                 Motivational Quotes App
+                              </h3>
+                           </h3>
+                           <p className="text-base font-medium text-body-color leading-relaxed my-4 text-slate-900 dark:text-yellow-400">
+                              This application uses quotes API to display random motivational quotes.
+                           </p>
+                           <button
+                              className="
+                              inline-block
+                              py-2
+                              px-7
+                              border border-[#E5E7EB]
+                              rounded-full
+                              text-base text-body-color
+                              font-medium
+                              hover:border-primary hover:bg-primary hover:text-white
+                              transition
+                              "
+                              onClick={toggleModal}
+                              >
+                           View Details
+                           </button>
+                        </div>
+                        <img
+                           src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                           alt="cards"
+                           className="w-full rounded-lg"
+                           />
+                        
+                     </div>
+                  </div>
+                  <div className="w-full md:w-1/2 xl:w-1/3 px-4 hover:scale-90 transition ease-in-out duration-500">
+                     <div className="rounded-lg overflow-hidden mb-10">
+                     <div className="p-4 text-center">
+                           <h3>
+                              <h3 className="text-3xl font-bold text-slate-900 dark:text-yellow-400">
+                                 Motivational Quotes App
+                              </h3>
+                           </h3>
+                           <p className="text-base font-medium text-body-color leading-relaxed my-4 text-slate-900 dark:text-yellow-400">
+                              This application uses quotes API to display random motivational quotes.
+                           </p>
+                           <button
+                              className="
+                              inline-block
+                              py-2
+                              px-7
+                              border border-[#E5E7EB]
+                              rounded-full
+                              text-base text-body-color
+                              font-medium
+                              hover:border-primary hover:bg-primary hover:text-white
+                              transition
+                              "
+                              onClick={toggleModal}
+                              >
+                           View Details
+                           </button>
+                        </div>
+                        <img
+                           src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                           alt="cards"
+                           className="w-full rounded-lg"
+                           />
+                        
+                     </div>
+                  </div>
                </div>
             </div>
-         </div>
-      </div>
-   </div>
-</section>
+         </section>
+         <Modal canShow={showModal} updateModalState={toggleModal} props={projects[0]} />
+      </ModalContext.Provider>
     )
 
 }
