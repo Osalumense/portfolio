d9fd093a8d3aa3cda56417f94617a2cfcc92d4d6
Stephen <harkugbeosaz@gmail.com>
2022-04-19 16:25:20 +0100

    Editing files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,17 @@ const Footer = () => {
         <div className="p-6">
             <div class="py-6 border-t border-slate-900 dark:border-gray-200 footer-copyright">
                 <p class="text-slate-900 dark:text-white flex justify-center items-center">
-                © Copyright Designed by <a class="duration-300 hover:text-yellow-400 ml-1" href="https://github.com/Osalumense" rel="noreferrer" target="_blank">Stephen</a> 
+                © Copyright Designed by 
+                <a 
+                    class="duration-300 
+                    hover:text-yellow-400 
+                    ml-1" 
+                    href="https://github.com/Osalumense" 
+                    rel="noreferrer" 
+                    target="_blank"
+                >
+                        Stephen
+                </a> 
                 </p>
             </div>
             <div
