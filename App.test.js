6e68f7b7933bbfe433747f476a540a4a0f578d83
Stephen <harkugbeosaz@gmail.com>
2022-05-11 15:34:54 -0700

    Removing unwanted files

--- App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Counter App 😃 title', () => {
-    render(<App />);
-    const titleElement = screen.getByText(/Portfolio/);
-    expect(titleElement).toBeInTheDocument();
-});
