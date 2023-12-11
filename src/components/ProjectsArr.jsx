2e543a855321c0b885279d3cbc51162a4101b98b
Akugbe Stephen <harkugbeosaz@gmail.com>
2023-12-12 00:01:00 +0100

    Update ProjectsArr.jsx

--- src/components/ProjectsArr.jsx
+++ src/components/ProjectsArr.jsx
@@ -14,19 +14,58 @@ export const projectsArr = [
         },
         {
            id: 2,
-           img: 'https://user-images.githubusercontent.com/43953425/175952547-ba4ccdb9-e001-4f51-ac81-01290dc02d7d.png',
-            overview: 'A website for a Medical Diagnostics Company',
+           img: 'https://user-images.githubusercontent.com/43953425/218332154-bb18af6d-f790-4507-af3e-6d1aef59cdd9.png',
+            overview: 'Event MS',
             details: {
-               title: 'Labogen Diagnostics',
-               description: 'The client needed a website that showcases their services and also some of their products for customer to gather information about them, seek their services and also immediately shop products. I designed the website using WordPress and WooCommerce',
-               vid: 'https://streamable.com/glcz4d?autoplay=1&loop=0',
-               techStack: ['WordPress', 'WooCommerce'],
-               ghLink: '',
-               liveLink: 'https://labogendiagnostics.com/',
+               title: 'Event MS',
+               description: 'Event MS is a powerful and flexible web application built with Laravel and Tailwind CSS. It simplifies the process of organizing events, whether they are paid or free, by providing a seamless registration and check-in experience for participants.',
+               vid: 'https://user-images.githubusercontent.com/43953425/218332162-7c21cb2a-a05b-475a-a9a1-6dd2f7ad3fa5.png',
+               techStack: ['HTML', 'Tailwind CSS', 'Laravel', 'jQuery', 'JavaScript'],
+               ghLink: 'https://github.com/Osalumense/event-ms',
+               // liveLink: 'https://osalumense.github.io/ludayhomepage/',
             }
         },
         {
-         id: 3,
+            id: 3,
+            img: 'https://user-images.githubusercontent.com/43953425/149300286-e1f3104f-99f9-49d6-83fe-425e2c7b3108.png',
+            overview: 'A website for a web development agency here in Nigeria',
+            details: {
+               title: 'MLD',
+               description: 'This is a website designed using the React framework and TailwindCSS for a web development agency in Nigeria',
+               vid: 'https://streamable.com/e/tek0i5?autoplay=1',
+               techStack: ['HTML', 'Tailwind CSS', 'React'],
+               ghLink: 'https://github.com/Osalumense/mld',
+               liveLink: 'https://mld.ng',
+            }
+         },
+         {
+            id: 4,
+            img: 'https://user-images.githubusercontent.com/43953425/159141624-b122104b-3215-44d5-9127-0c96ba33cbd9.PNG',
+            overview: 'This portfolio website was designed using React',
+            details: {
+               title: 'My Portfolio Website',
+               description: 'This is my portfolio website designed using the React framework and TailwindCSS. In this project, I made use of the Context API, React hooks (useState, useEffect).',
+               vid: 'https://streamable.com/e/v1tgvt?autoplay=1',
+               techStack: ['HTML', 'Tailwind CSS', 'React', 'Netlify'],
+               ghLink: 'https://github.com/Osalumense/portfolio',
+               liveLink: 'https://akugbe-stephen.netlify.app/',
+            }
+         },
+         {
+            id: 5,
+            img: 'https://github.com/Osalumense/hcs/assets/43953425/8348a774-e77e-4458-be4e-f9628be473d3',
+            overview: 'A website for a fashion brand in Nigeria',
+            details: {
+               title: 'Rare Gem Collectibles',
+               description: 'The client needed a website that showcases their services and also some of their products for customer to gather information about them and immediately shop products. I designed the website using WordPress and WooCommerce',
+               vid: 'https://github-production-user-asset-6210df.s3.amazonaws.com/43953425/289691215-85db861f-df6b-479f-960d-675dfcbc34a2.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231211%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231211T223527Z&X-Amz-Expires=300&X-Amz-Signature=11e346e82701e67be6563f1ef3e7a4273e69530d5eb5c27656a5c1f8110395bb&X-Amz-SignedHeaders=host&actor_id=43953425&key_id=0&repo_id=454053524',
+               techStack: ['WordPress', 'WooCommerce'],
+               ghLink: '',
+               liveLink: 'https://raregemcollectibles.com/',
+            }
+         },
+        {
+         id: 6,
          img: 'https://user-images.githubusercontent.com/43953425/150501075-ced19a3a-1181-418a-a20b-88942267cf8b.PNG',
          overview: 'A small web application that generates random motivational quotes to keep you going',
          details: {
@@ -38,45 +77,19 @@ export const projectsArr = [
             liveLink: 'https://motivational-quotes-app.vercel.app/',
          }
       },
-        {
-            id: 4,
-            img: 'https://user-images.githubusercontent.com/43953425/149300286-e1f3104f-99f9-49d6-83fe-425e2c7b3108.png',
-            overview: 'A react web template designed for small businesses and startups ',
-            details: {
-               title: 'React Web Template',
-               description: 'This is a website designed using the React framework and TailwindCSS for small business and startups',
-               vid: 'https://streamable.com/e/tek0i5?autoplay=1',
-               techStack: ['HTML', 'Tailwind CSS', 'React', 'Netlify'],
-               ghLink: 'https://github.com/Osalumense/mld',
-               liveLink: 'https://mld-prototype.netlify.app/',
-            }
-         },
-         {
-            id: 5,
-            img: 'https://user-images.githubusercontent.com/43953425/159141624-b122104b-3215-44d5-9127-0c96ba33cbd9.PNG',
-            overview: 'This portfolio website designed using React',
-            details: {
-               title: 'My Portfolio Website',
-               description: 'This is my portfolio website designed using the React framework and TailwindCSS. In this project, I made use of the Context API, React hooks (useState, useEffect).',
-               vid: 'https://streamable.com/e/v1tgvt?autoplay=1',
-               techStack: ['HTML', 'Tailwind CSS', 'React', 'Netlify'],
-               ghLink: 'https://github.com/Osalumense/portfolio',
-               liveLink: 'https://akugbe-stephen.netlify.app/',
-            }
-         },
-         {
-            id: 6,
-            img: 'https://user-images.githubusercontent.com/43953425/159142872-6c966c1d-7e6c-4250-a292-d91a1566fc3f.PNG',
-            overview: 'A simple web development agency website',
-            details: {
-               title: 'Web development agency landing page',
-               description: 'This is a simple web development agency template designed using Bootstrap and Javacsript.',
-               vid: 'https://streamable.com/e/9613b7?autoplay=1',
-               techStack: ['HTML', 'Bootstrap', 'Javascipt', 'jQuery'],
-               ghLink: 'https://github.com/Osalumense/ludayhomepage/tree/main',
-               liveLink: 'https://osalumense.github.io/ludayhomepage/',
-            }
-         }        
+      {
+         id: 7,
+         img: 'https://user-images.githubusercontent.com/43953425/151820511-40400477-d480-42f0-9364-f274bd43db8a.PNG',
+         overview: 'A small web application that generates random motivational quotes to keep you going',
+         details: {
+            title: 'HCS (Health COunselling System',
+            description: 'A mental health counselling application developed in the Laravel framework, the appplication enables users to enjoy counselling sessions from the comfort of their homes either through chat and video calls.',
+            vid: 'https://github-production-user-asset-6210df.s3.amazonaws.com/43953425/289694818-b9c1cac0-6c2a-4bad-bcc7-c9c3c311f326.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231211%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231211T225755Z&X-Amz-Expires=300&X-Amz-Signature=f1201cdb0f7d752cf559f4f8d71a86933d3f3735b7d527b1ccbd61fa92cd327f&X-Amz-SignedHeaders=host&actor_id=43953425&key_id=0&repo_id=454053524',
+            techStack: ['HTML', 'Laravel', 'jQuery', 'JavaScript'],
+            ghLink: 'https://github.com/Osalumense/hcs',
+            liveLink: '',
+         }
+      },
      ]
 
 
